d9012216a3996ec66d713dcca543bdcca4dfcef1
Washi <washi1337@protonmail.com>
2019-06-10 17:46:55 +0200

    IInstruction model base.

---
+++ src/Core/Echo.Core/Emulation/IProgramState.cs
@@ -0,0 +1,9 @@
+namespace Echo.Core.Emulation
+{
+    /// <summary>
+    /// Represents a snapshot of the state of the program in a particular point of execution.
+    /// </summary>
+    public interface IProgramState
+    {
+    }
+}
