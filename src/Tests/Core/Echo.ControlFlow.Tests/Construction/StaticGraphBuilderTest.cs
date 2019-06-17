e3994c0fc477dff15f9953c8c3e1b0819d796b73
Washi <washi1337@protonmail.com>
2019-06-17 21:31:28 +0200

    Fixed loop test for static cfg builder.

--- src/Tests/Core/Echo.ControlFlow.Tests/Construction/StaticGraphBuilderTest.cs
+++ src/Tests/Core/Echo.ControlFlow.Tests/Construction/StaticGraphBuilderTest.cs
@@ -68,35 +68,51 @@ namespace Echo.ControlFlow.Tests.Construction
         {
             var instructions = new[]
             {
+                // Entrypoint
                 DummyInstruction.Op(0, 0, 1),
-                DummyInstruction.Op(0, 1, 0),
-                DummyInstruction.Jmp(4, ),
+                DummyInstruction.Op(1, 1, 0),
+                DummyInstruction.Jmp(2, 7),
                 
-                DummyInstruction.Op(0, 0, 1),
-                DummyInstruction.Op(0, 0, 1),
-                DummyInstruction.Op(0, 2, 1),
-                DummyInstruction.Op(0, 1, 0),
+                // Loop body
+                DummyInstruction.Op(3, 0, 1),
+                DummyInstruction.Op(4, 0, 1),
+                DummyInstruction.Op(5, 2, 1),
+                DummyInstruction.Op(6, 1, 0),
                 
-                DummyInstruction.Op(0, 0, 1),
-                DummyInstruction.Op(0, 0, 1),
-                DummyInstruction.Op(0, 2, 1),
-                DummyInstruction.JmpCond(1, 2),
+                // Loop header
+                DummyInstruction.Op(7, 0, 1),
+                DummyInstruction.Op(8, 0, 1),
+                DummyInstruction.Op(9, 2, 1),
+                DummyInstruction.JmpCond(10, 3),
                 
-                DummyInstruction.Op(2, 0, 0),
-                DummyInstruction.Ret(7)
+                // Exit
+                DummyInstruction.Op(11, 0, 0),
+                DummyInstruction.Ret(12)
             };
             
             var list = new InstructionList<DummyInstruction>(instructions);
             var builder = new DummyStaticGraphBuilder(list);
             var graph = builder.ConstructFlowGraph(0);
-
-
+            
             Assert.Equal(4, graph.Nodes.Count);
-            Assert.Single(graph.Entrypoint.ConditionalEdges);
-            Assert.NotNull(graph.Entrypoint.FallThroughEdge);
-            Assert.Equal(
-                graph.Entrypoint.FallThroughNeighbour.FallThroughNeighbour, 
-                graph.Entrypoint.ConditionalEdges.First().Target.FallThroughNeighbour);
+            
+            // Entrypoint.
+            Assert.NotNull(graph.Entrypoint.FallThroughNeighbour);
+            Assert.Empty(graph.Entrypoint.ConditionalEdges);
+            
+            // Loop header
+            var loopHeader = graph.Entrypoint.FallThroughNeighbour;
+            Assert.NotNull(loopHeader.FallThroughEdge);
+            Assert.Single(loopHeader.ConditionalEdges);
+            
+            // Loop body
+            var loopBody = loopHeader.ConditionalEdges.First().Target;
+            Assert.Equal(loopHeader, loopBody.FallThroughNeighbour);
+            Assert.Empty(loopBody.ConditionalEdges);
+            
+            // Exit
+            var exit = loopHeader.FallThroughNeighbour;
+            Assert.Empty(exit.GetOutgoingEdges());
         }
     }
 }
