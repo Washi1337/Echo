6aed4045ba3c37e0451f2c2f602c69d689707620
Washi <washi1337@protonmail.com>
2019-07-25 22:06:29 +0200

    NodeComparer -> DominatorNodeComparer

--- src/Core/Echo.ControlFlow/Serialization/StructuredFlow/StructuredFlowGenerator.cs
+++ src/Core/Echo.ControlFlow/Serialization/StructuredFlow/StructuredFlowGenerator.cs
@@ -12,9 +12,9 @@ namespace Echo.ControlFlow.Serialization.StructuredFlow
     /// <typeparam name="TInstruction">The type of instructions that the control flow graph stores.</typeparam>
     public class StructuredFlowGenerator<TInstruction> : IStructuredFlowGenerator<TInstruction>
     {
-        private sealed class NodeComparer : IComparer<DominatorTreeNode>
+        private sealed class DominatorNodeComparer : IComparer<DominatorTreeNode>
         {
-            public DominatorTreeNode CurrentTreeNode
+            public DominatorTreeNode CurrentParent
             {
                 get;
                 set;
@@ -35,7 +35,7 @@ namespace Echo.ControlFlow.Serialization.StructuredFlow
                     return !node1.GetOutgoingEdges().Any() ? 0 : -1;
                 
                 // Prioritize fallthrough edges.
-                var fallThroughEdge = CurrentTreeNode.OriginalNode.GetFallThroughEdge();
+                var fallThroughEdge = CurrentParent.OriginalNode.GetFallThroughEdge();
                 if (fallThroughEdge.Target == node1)
                     return -1;
                 if (fallThroughEdge.Target == node2)
@@ -49,7 +49,7 @@ namespace Echo.ControlFlow.Serialization.StructuredFlow
         public ScopeBlock<TInstruction> Generate(ControlFlowGraph<TInstruction> cfg)
         {
             var dominatorTree = DominatorTree.FromGraph(cfg);
-            var comparer =new NodeComparer();
+            var comparer =new DominatorNodeComparer();
             
             var scopes = new Stack<ScopeBlock<TInstruction>>();
             scopes.Push(new ScopeBlock<TInstruction>());
@@ -65,12 +65,13 @@ namespace Echo.ControlFlow.Serialization.StructuredFlow
                 // Add current node to the current block.
                 scopes.Peek().Blocks.Add(currentNode.Contents);
 
-                comparer.CurrentTreeNode = current;
+                // Obtain sorted list of children.
+                comparer.CurrentParent = current;
                 var children = GetSortedChildren(current, comparer);
 
+                // Schedule them for processing (= push them in reverse order).
                 foreach (var child in children.Reverse())
                     agenda.Push(child);
-
             }
                 
             return scopes.Pop();
