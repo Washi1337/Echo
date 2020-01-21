f6435190191d75e8bb728473a19c801a3637541c
Washi <washi1337@protonmail.com>
2020-01-21 15:30:28 +0100

    Add AsmResolver platform.

---
+++ src/Platforms/Echo.Platforms.AsmResolver/AsmResolverCilArchitecture.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.Core.Code;
+
+namespace Echo.Platforms.AsmResolver
+{
+    /// <summary>
+    /// Provides a description of the CIL instruction set architecture (ISA) that is modelled by AsmResolver.   
+    /// </summary>
+    public class AsmResolverCilArchitecture : IInstructionSetArchitecture<CilInstruction>
+    {
+        /// <summary>
+        /// Gets a singleton instance of this architecture description.
+        /// </summary>
+        public static AsmResolverCilArchitecture Instance
+        {
+            get;
+        } = new AsmResolverCilArchitecture();
+
+        /// <summary>
+        /// Gets the default static successor resolution engine for this architecture.
+        /// </summary>
+        public AsmResolverCilSuccessorResolver SuccessorResolver
+        {
+            get;
+        } = new AsmResolverCilSuccessorResolver();
+
+        /// <inheritdoc />
+        public long GetOffset(CilInstruction instruction) =>
+            instruction.Offset;
+
+        /// <inheritdoc />
+        public string GetMnemonic(CilInstruction instruction) =>
+            instruction.OpCode.Mnemonic;
+
+        /// <inheritdoc />
+        public int GetOperandCount(CilInstruction instruction) =>
+            instruction.Operand == null ? 0 : 1;
+
+        /// <inheritdoc />
+        public object GetOperand(CilInstruction instruction, int index) =>
+            instruction.Operand != null && index == 0 ? instruction.Operand : throw new IndexOutOfRangeException();
+
+        /// <inheritdoc />
+        public int GetSize(CilInstruction instruction) =>
+            instruction.Size;
+
+        /// <inheritdoc />
+        public byte[] GetOpCodeBytes(CilInstruction instruction)
+        {
+            var opCode = instruction.OpCode;
+            return opCode.Size == 2
+                ? new[] { opCode.Byte1, opCode.Byte2 }
+                : new[] { opCode.Byte1 };
+        }
+
+        /// <inheritdoc />
+        public byte[] GetOperandBytes(CilInstruction instruction)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public byte[] GetInstructionBytes(CilInstruction instruction)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public int GetStackPushCount(CilInstruction instruction)
+        {
+            return instruction.GetStackPushCount();
+        }
+
+        /// <inheritdoc />
+        public int GetStackPopCount(CilInstruction instruction)
+        {
+            // TODO: incorporate the parent method body somehow.
+            return instruction.GetStackPopCount(null);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<IVariable> GetReadVariables(CilInstruction instruction)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<IVariable> GetWrittenVariables(CilInstruction instruction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Platforms/Echo.Platforms.AsmResolver/AsmResolverCilSuccessorResolver.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.ControlFlow;
+using Echo.ControlFlow.Construction;
+
+namespace Echo.Platforms.AsmResolver
+{
+    public class AsmResolverCilSuccessorResolver : IStaticSuccessorResolver<CilInstruction>
+    {
+        public static AsmResolverCilSuccessorResolver Instance
+        {
+            get;
+        } = new AsmResolverCilSuccessorResolver();
+        
+        public ICollection<SuccessorInfo> GetSuccessors(CilInstruction instruction)
+        {
+            var result = new List<SuccessorInfo>(1);
+
+            switch (instruction.OpCode.FlowControl)
+            {
+                case CilFlowControl.Break:
+                case CilFlowControl.Call:
+                case CilFlowControl.Meta:
+                case CilFlowControl.Next:
+                    result.Add(CreateFallThrough(instruction));
+                    break;
+                
+                case CilFlowControl.Branch:
+                    var label = (ICilLabel) instruction.Operand;
+                    result.Add(CreateConditional(label));
+                    break;
+                
+                case CilFlowControl.ConditionalBranch:
+                    switch (instruction.Operand)
+                    {
+                        case ICilLabel singleTarget:
+                            result.Add(CreateConditional(singleTarget));
+                            break;
+                        
+                        case IEnumerable<ICilLabel> multipleTargets:
+                            foreach (var target in multipleTargets)
+                                result.Add(CreateConditional(target));
+                            break;
+                    }
+
+                    result.Add(CreateFallThrough(instruction));
+                    break;
+                
+                case CilFlowControl.Phi:
+                    throw new NotSupportedException();
+                
+                case CilFlowControl.Return:
+                case CilFlowControl.Throw:
+                    break;
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            return result;
+        }
+
+        private static SuccessorInfo CreateFallThrough(CilInstruction instruction)
+        {
+            return new SuccessorInfo(instruction.Offset + instruction.Size, EdgeType.FallThrough);
+        }
+
+        private static SuccessorInfo CreateConditional(ICilLabel singleTarget)
+        {
+            return new SuccessorInfo(singleTarget.Offset, EdgeType.Conditional);
+        }
+    }
+}
