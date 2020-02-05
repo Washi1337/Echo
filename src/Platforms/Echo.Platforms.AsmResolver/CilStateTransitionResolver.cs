f6e27186737c8c2a42a1b58a9fbb77f0fc77b469
Washi <washi1337@protonmail.com>
2020-02-05 20:52:47 +0100

    CIL state transition resolver.

---
+++ src/Platforms/Echo.Platforms.AsmResolver/CilStateTransitionResolver.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.ControlFlow;
+using Echo.ControlFlow.Construction.Symbolic;
+using Echo.DataFlow.Emulation;
+
+namespace Echo.Platforms.AsmResolver
+{
+    public class CilStateTransitionResolver : StateTransitionResolverBase<CilInstruction>
+    {
+        public CilStateTransitionResolver()
+            : base(CilArchitecture.Instance)
+        {
+        }
+
+        public override IEnumerable<StateTransition<CilInstruction>> GetTransitions(SymbolicProgramState<CilInstruction> currentState, CilInstruction instruction)
+        {
+            switch (instruction.OpCode.FlowControl)
+            {
+                case CilFlowControl.Branch:
+                    return GetUnconditionalBranchTransitions(currentState, instruction);
+
+                case CilFlowControl.ConditionalBranch:
+                    return GetConditionalBranchTransitions(currentState, instruction);
+                
+                case CilFlowControl.Call:
+                case CilFlowControl.Meta:
+                case CilFlowControl.Next:
+                case CilFlowControl.Break:
+                    return GetFallthroughTransitions(currentState, instruction);
+                
+                case CilFlowControl.Return:
+                case CilFlowControl.Throw:
+                    return Enumerable.Empty<StateTransition<CilInstruction>>();
+
+                case CilFlowControl.Phi:
+                    throw new NotSupportedException();
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private IEnumerable<StateTransition<CilInstruction>> GetFallthroughTransitions(
+            SymbolicProgramState<CilInstruction> currentState, CilInstruction instruction)
+        {
+            var nextState = currentState.Copy();
+            ApplyDefaultBehaviour(nextState, instruction);
+
+            var transition = new StateTransition<CilInstruction>(nextState, ControlFlowEdgeType.FallThrough);
+            return new[]
+            {
+                transition
+            };
+        }
+
+        private IEnumerable<StateTransition<CilInstruction>> GetUnconditionalBranchTransitions(
+            SymbolicProgramState<CilInstruction> currentState, CilInstruction instruction)
+        {           
+            var nextState = currentState.Copy();
+            ApplyDefaultBehaviour(nextState, instruction);
+            
+            nextState.ProgramCounter = ((ICilLabel) instruction.Operand).Offset;
+
+            var transition = new StateTransition<CilInstruction>(nextState, ControlFlowEdgeType.FallThrough);
+            return new[]
+            {
+                transition
+            };
+        }
+
+        private IEnumerable<StateTransition<CilInstruction>> GetConditionalBranchTransitions(
+            SymbolicProgramState<CilInstruction> currentState, CilInstruction instruction)
+        {
+            var baseNextState = currentState.Copy();
+            ApplyDefaultBehaviour(baseNextState, instruction);
+
+            var result = new List<StateTransition<CilInstruction>>
+            {
+                new StateTransition<CilInstruction>(baseNextState, ControlFlowEdgeType.FallThrough)
+            };
+
+            switch (instruction.Operand)
+            {
+                case ICilLabel singleTarget:
+                    var branchState = baseNextState.Copy();
+                    branchState.ProgramCounter = singleTarget.Offset;
+                    result.Add(new StateTransition<CilInstruction>(branchState, ControlFlowEdgeType.Conditional));
+                    break;
+                            
+                case IEnumerable<ICilLabel> multipleTargets:
+                    foreach (var target in multipleTargets)
+                    {
+                        var nextBranchState = baseNextState.Copy();
+                        nextBranchState.ProgramCounter = target.Offset;
+                        result.Add(new StateTransition<CilInstruction>(nextBranchState, ControlFlowEdgeType.Conditional));
+                    }
+
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- src/Platforms/Echo.Platforms.AsmResolver/CilStaticSuccessorResolver.cs
+++ src/Platforms/Echo.Platforms.AsmResolver/CilStaticSuccessorResolver.cs
@@ -7,6 +7,9 @@ using Echo.ControlFlow.Construction.Static;
 
 namespace Echo.Platforms.AsmResolver
 {
+    /// <summary>
+    /// Provides an implementation of <see cref="IStaticSuccessorResolver{CilInstruction}"/>
+    /// </summary>
     public class CilStaticSuccessorResolver : IStaticSuccessorResolver<CilInstruction>
     {
         public static CilStaticSuccessorResolver Instance
@@ -16,58 +19,74 @@ namespace Echo.Platforms.AsmResolver
         
         public ICollection<SuccessorInfo> GetSuccessors(CilInstruction instruction)
         {
-            var result = new List<SuccessorInfo>(1);
-
             switch (instruction.OpCode.FlowControl)
             {
                 case CilFlowControl.Break:
                 case CilFlowControl.Call:
                 case CilFlowControl.Meta:
                 case CilFlowControl.Next:
-                    result.Add(CreateFallThrough(instruction));
-                    break;
+                    return GetFallThroughTransitions(instruction);
                 
                 case CilFlowControl.Branch:
-                    var label = (ICilLabel) instruction.Operand;
-                    result.Add(new SuccessorInfo(label.Offset, ControlFlowEdgeType.FallThrough));
-                    break;
+                    return GetUnconditionalBranchTransitions(instruction);
                 
                 case CilFlowControl.ConditionalBranch:
-                    switch (instruction.Operand)
-                    {
-                        case ICilLabel singleTarget:
-                            result.Add(CreateConditional(singleTarget));
-                            break;
-                        
-                        case IEnumerable<ICilLabel> multipleTargets:
-                            foreach (var target in multipleTargets)
-                                result.Add(CreateConditional(target));
-                            break;
-                    }
-
-                    result.Add(CreateFallThrough(instruction));
-                    break;
+                    return GetConditionalBranchTransitions(instruction);
                 
                 case CilFlowControl.Phi:
                     throw new NotSupportedException();
                 
                 case CilFlowControl.Return:
                 case CilFlowControl.Throw:
-                    break;
+                    return Array.Empty<SuccessorInfo>();
                 
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-            
+        }
+
+        private static ICollection<SuccessorInfo> GetFallThroughTransitions(CilInstruction instruction)
+        {
+            return new[]
+            {
+                CreateFallThroughTransition(instruction)
+            };
+        }
+
+        private static ICollection<SuccessorInfo> GetUnconditionalBranchTransitions(CilInstruction instruction)
+        {
+            var label = (ICilLabel) instruction.Operand;
+            return new[]
+            {
+                new SuccessorInfo(label.Offset, ControlFlowEdgeType.FallThrough)
+            };
+        }
+
+        private static ICollection<SuccessorInfo> GetConditionalBranchTransitions(CilInstruction instruction)
+        {
+            var result = new List<SuccessorInfo>(1);
+            switch (instruction.Operand)
+            {
+                case ICilLabel singleTarget:
+                    result.Add(CreateConditionalTransition(singleTarget));
+                    break;
+
+                case IEnumerable<ICilLabel> multipleTargets:
+                    foreach (var target in multipleTargets)
+                        result.Add(CreateConditionalTransition(target));
+                    break;
+            }
+
+            result.Add(CreateFallThroughTransition(instruction));
             return result;
         }
 
-        private static SuccessorInfo CreateFallThrough(CilInstruction instruction)
+        private static SuccessorInfo CreateFallThroughTransition(CilInstruction instruction)
         {
             return new SuccessorInfo(instruction.Offset + instruction.Size, ControlFlowEdgeType.FallThrough);
         }
 
-        private static SuccessorInfo CreateConditional(ICilLabel singleTarget)
+        private static SuccessorInfo CreateConditionalTransition(ICilLabel singleTarget)
         {
             return new SuccessorInfo(singleTarget.Offset, ControlFlowEdgeType.Conditional);
         }
