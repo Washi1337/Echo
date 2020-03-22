98d48f092fe20e0fdb1e2dc693631f9b2a6d16d6
Washi <washi1337@protonmail.com>
2020-03-22 18:04:33 +0100

    New implementation for finding SCCs

--- src/Core/Echo.Core/Graphing/Analysis/Connectivity/ComponentDetector.cs
+++ src/Core/Echo.Core/Graphing/Analysis/Connectivity/ComponentDetector.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using Echo.Core.Graphing.Analysis.Traversal;
-using Echo.Core.Graphing;
 
 namespace Echo.Core.Graphing.Analysis.Connectivity
 {
@@ -17,34 +15,67 @@ namespace Echo.Core.Graphing.Analysis.Connectivity
         /// <returns>A collection of sets representing the strongly connected components.</returns>
         public static ICollection<ISet<INode>> FindStronglyConnectedComponents(this IGraph graph)
         {
-            var traversal = new DepthFirstTraversal();
-            var recorder = new PostOrderRecorder(traversal);
-
-            traversal.Run(graph.GetNodes().First());
-
+            // This is an implementation of the Kosaraju’s algorithm, but altered to work on disconnected graphs
+            // and the transposition of the graph is replaced with looking at the predecessors rather than the
+            // successors of the current node in the depth first search. 
+            
+            var nodes = new HashSet<INode>(graph.GetNodes());
             var visited = new HashSet<INode>();
             var result = new List<ISet<INode>>();
-            foreach (var node in recorder.GetOrder().Reverse())
-            {
-                if (!visited.Contains(node))
-                {
-                    var subTraversal = new DepthFirstTraversal(true);
-                    var component = new HashSet<INode>();
-                    subTraversal.NodeDiscovered += (sender, args) =>
-                    {
-                        if (visited.Add(args.NewNode))
-                        {
-                            args.ContinueExploring = true;
-                            component.Add(args.NewNode);
-                        }
-                    };
-                    subTraversal.Run(node);
 
-                    result.Add(component);
+            while (nodes.Count != 0)
+            {
+                var start = nodes.First();
+
+                var stack = new Stack<INode>();
+                FillOrder(new HashSet<INode>(), stack, start);
+                
+                while (stack.Count > 0)
+                {
+                    var current = stack.Pop();
+                    nodes.Remove(current);
+                    if (!visited.Contains(current))
+                        result.Add(ReverseDepthFirstSearch(visited, current));
                 }
             }
 
             return result;
-        }       
+        }
+
+        private static void FillOrder(ISet<INode> visited, Stack<INode> stack, INode start)
+        {
+            visited.Add(start);
+            
+            foreach (var neighbour in start.GetSuccessors())
+            {
+                if (!visited.Contains(neighbour))
+                    FillOrder(visited, stack, neighbour);
+            }
+
+            stack.Push(start);
+        }
+
+        private static ISet<INode> ReverseDepthFirstSearch(ISet<INode> visited, INode start)
+        {
+            var result = new HashSet<INode>();
+            
+            var agenda = new Stack<INode>();
+            agenda.Push(start);
+
+            while (agenda.Count > 0)
+            {
+                var current = agenda.Pop();
+                result.Add(current);
+                visited.Add(current);
+
+                foreach (var predecessor in current.GetPredecessors())
+                {
+                    if (!visited.Contains(predecessor))
+                        agenda.Push(predecessor);
+                }
+            }
+
+            return result;
+        }
     }
 }
