a5c1a829ce881bc469aa410490211c326268e1b7
Washi <washi1337@protonmail.com>
2020-03-30 00:04:23 +0200

    Add more region validity tests.

--- test/Core/Echo.ControlFlow.Tests/Regions/ControlFlowRegionCollectionTest.cs
+++ test/Core/Echo.ControlFlow.Tests/Regions/ControlFlowRegionCollectionTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Echo.ControlFlow.Regions;
 using Echo.Platforms.DummyPlatform;
 using Xunit;
@@ -27,6 +28,49 @@ namespace Echo.ControlFlow.Tests.Regions
             Assert.Same(graph, region.ParentRegion);
             graph.Regions.Remove(region);
             Assert.Null(region.ParentRegion);
+        }
+
+        [Fact]
+        public void ClearAllSubRegionsShouldUnsetAllParents()
+        {
+            var graph = new ControlFlowGraph<int>(IntArchitecture.Instance);
+
+            var regions = new[]
+            {
+                new BasicControlFlowRegion<int>(), new BasicControlFlowRegion<int>(), new BasicControlFlowRegion<int>(),
+            };
+
+            foreach (var region in regions)
+                graph.Regions.Add(region);
+            
+            Assert.All(regions, r => Assert.Same(graph, r.ParentRegion));
+            
+            graph.Regions.Clear();
+            
+            Assert.All(regions, r => Assert.Null(r.ParentRegion)); 
+        }
+
+        [Fact]
+        public void AddingRegionSecondTimeShouldThrow()
+        { 
+            var graph = new ControlFlowGraph<int>(IntArchitecture.Instance);
+            var region = new BasicControlFlowRegion<int>();
+
+            graph.Regions.Add(region);
+
+            Assert.Throws<ArgumentException>(() => graph.Regions.Add(region));
+        }
+
+        [Fact]
+        public void AddingRegionOfAnotherGraphShouldThrow()
+        { 
+            var graph1 = new ControlFlowGraph<int>(IntArchitecture.Instance);
+            var graph2 = new ControlFlowGraph<int>(IntArchitecture.Instance);
+            var region = new BasicControlFlowRegion<int>();
+
+            graph1.Regions.Add(region);
+
+            Assert.Throws<ArgumentException>(() => graph2.Regions.Add(region));
         }
     }
 }
