1402eff4df746c92d996792634f9a997318f1977
Washi <washi1337@protonmail.com>
2020-04-13 13:57:34 +0200

    Add LdcR4/8 handlers.

---
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/Dispatch/Constants/LdcR4.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.Concrete.Emulation;
+using Echo.Concrete.Emulation.Dispatch;
+using Echo.Concrete.Values.ValueType;
+
+namespace Echo.Platforms.AsmResolver.Emulation.Dispatch.Constants
+{
+    /// <summary>
+    /// Provides a handler for instructions with the ldc.r4 operation code.
+    /// </summary>
+    public class LdcR4 : FallThroughOpCodeHandler
+    {
+        /// <inheritdoc />
+        public override IReadOnlyCollection<CilCode> SupportedOpCodes => new[]
+        {
+            CilCode.Ldc_R4
+        };
+
+        /// <inheritdoc />
+        public override DispatchResult Execute(ExecutionContext context, CilInstruction instruction)
+        {
+            context.ProgramState.Stack.Push(new Float32Value((float) instruction.Operand));
+            return base.Execute(context, instruction);
+        }
+    }
+}
