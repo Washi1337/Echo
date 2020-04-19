1184a384f4d88052bd6612e0580fe34068128466
Washi <washi1337@protonmail.com>
2020-04-19 19:49:09 +0200

    Add Mul opcode.

--- src/Platforms/Echo.Platforms.AsmResolver/Emulation/Dispatch/ControlFlow/Ret.cs
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/Dispatch/ControlFlow/Ret.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AsmResolver.PE.DotNet.Cil;
 using Echo.Concrete.Emulation;
@@ -19,6 +20,23 @@ namespace Echo.Platforms.AsmResolver.Emulation.Dispatch.ControlFlow
         /// <inheritdoc />
         public DispatchResult Execute(ExecutionContext context, CilInstruction instruction)
         {
+            var environment = context.GetService<ICilRuntimeEnvironment>();
+            
+            // Note that we do not actually pop the value here if the architecture says so. This is done intentionally
+            // to allow the caller to obtain the return value from the stack.
+            
+            // We still want to check that the return is valid within the current context. That is, if it is expected
+            // to return a value, the stack should contain exactly one value, and no values otherwise.
+            
+            int popCount = environment.Architecture.GetStackPopCount(instruction);
+            if (context.ProgramState.Stack.Size != popCount)
+            {
+                return new DispatchResult
+                {
+                    Exception = new InvalidProgramException()
+                };
+            }
+            
             return new DispatchResult
             {
                 HasTerminated = true
