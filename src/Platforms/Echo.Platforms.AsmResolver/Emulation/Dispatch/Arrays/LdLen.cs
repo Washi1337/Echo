53855268ac689e1ba4b8a081d3c338431a710529
Washi <washi1337@protonmail.com>
2020-05-05 12:46:50 +0200

    Add ldlen opcode handler.

---
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/Dispatch/Arrays/LdLen.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.Concrete.Emulation;
+using Echo.Concrete.Emulation.Dispatch;
+using Echo.Platforms.AsmResolver.Emulation.Values;
+using Echo.Platforms.AsmResolver.Emulation.Values.Cli;
+
+namespace Echo.Platforms.AsmResolver.Emulation.Dispatch.Arrays
+{
+    /// <summary>
+    /// Provides a handler for instructions with the <see cref="CilOpCodes.Ldlen"/> operation code.
+    /// </summary>
+    public class LdLen : FallThroughOpCodeHandler
+    {
+        /// <inheritdoc />
+        public override IReadOnlyCollection<CilCode> SupportedOpCodes => new[]
+        {
+            CilCode.Ldlen
+        };
+
+        /// <inheritdoc />
+        public override DispatchResult Execute(ExecutionContext context, CilInstruction instruction)
+        {
+            var environment = context.GetService<ICilRuntimeEnvironment>();
+            var stack = context.ProgramState.Stack;
+            
+            NativeIntegerValue lengthValue;
+            
+            var argument = stack.Pop();
+            switch (argument)
+            {
+                case OValue nullValue when nullValue.IsZero.GetValueOrDefault():
+                    return new DispatchResult(new NullReferenceException());
+                
+                case OValue { ObjectValue: IDotNetArrayValue arrayValue }:
+                    lengthValue = new NativeIntegerValue(arrayValue.Length, environment.Is32Bit);
+                    break;
+                
+                case OValue _:
+                    // Undefined behaviour when this happens on any other kind of object.
+                    lengthValue = new NativeIntegerValue(0, 0, environment.Is32Bit);
+                    break;
+
+                default:
+                    return DispatchResult.InvalidProgram();
+            }
+            
+            stack.Push(lengthValue);
+            return base.Execute(context, instruction);
+        }
+        
+    }
+}
