e29b3d7af44d180f363e72474d3787c6bd2f7cf0
Washi <washi1337@protonmail.com>
2020-05-09 00:19:54 +0200

    Add Cil VM test with loop and variables.

--- test/Platforms/Echo.Platforms.AsmResolver.Tests/Emulation/CilVirtualMachineTest.cs
+++ test/Platforms/Echo.Platforms.AsmResolver.Tests/Emulation/CilVirtualMachineTest.cs
@@ -1,39 +1,48 @@
-using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using AsmResolver.DotNet;
-using AsmResolver.DotNet.Signatures;
-using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
 using Echo.Concrete.Values.ValueType;
 using Echo.Platforms.AsmResolver.Emulation;
-using Echo.Platforms.AsmResolver.Tests.Mock;
 using Xunit;
 
 namespace Echo.Platforms.AsmResolver.Tests.Emulation
 {
     public class CilVirtualMachineTest 
     {
-        private static int TestMethod()
+        public static int TestMethod()
         {
             return 3 + 4;
         }
+
+        public static int TestLoop()
+        {
+            int result = 0;
+            for (int i = 0; i < 10; i++)
+                result += i;
+            return result;
+        }
         
-        [Fact]
-        public void SimpleMethodBody()
+        [Theory]
+        [InlineData(nameof(TestMethod))]
+        [InlineData(nameof(TestLoop))]
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        public void SimpleMethodBody(string methodName)
         {
             // Look up method.
             var module = ModuleDefinition.FromFile(typeof(CilVirtualMachineTest).Assembly.Location);
-            var method = module
-                .TopLevelTypes.First(t => t.Name == nameof(CilVirtualMachineTest))
-                .Methods.First(m => m.Name == nameof(TestMethod));
+            var reflectionMethod = typeof(CilVirtualMachineTest).GetMethod(methodName);
+            var asmResMethod = (MethodDefinition) module.LookupMember(reflectionMethod.MetadataToken);
             
             // Create new virtual machine.
-            var vm = new CilVirtualMachine(method.CilMethodBody, true);
+            var vm = new CilVirtualMachine(asmResMethod.CilMethodBody, true);
             
             // Execute.
             var result = vm.Execute(CancellationToken.None);
             
             // Inspect return value.
-            Assert.Equal(new Integer32Value(7), result.ReturnValue);
+            int expectedResult = (int) reflectionMethod.Invoke(null, null);
+            Assert.Equal(new Integer32Value(expectedResult), result.ReturnValue);
         }
+        
     }
 }
