eda37540952c8bc64f09e560d9391206df8df59a
Washi <washi1337@protonmail.com>
2020-05-16 22:59:55 +0200

    Add ReturnUnknownMethodInvoker class

---
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/Invocation/ReturnUnknownMethodInvoker.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using AsmResolver.DotNet;
+using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+using Echo.Platforms.AsmResolver.Emulation.Values.Cli;
+
+namespace Echo.Platforms.AsmResolver.Emulation.Invocation
+{
+    /// <summary>
+    /// Provides an implementation for an <see cref="IMethodInvoker"/> that always returns an unknown value when the
+    /// called method is non-void.
+    /// </summary>
+    public class AlwaysReturnUnknownMethodInvoker : IMethodInvoker
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="AlwaysReturnUnknownMethodInvoker"/> class.
+        /// </summary>
+        /// <param name="unknownValueFactory">The factory responsible for constructing the unknown values.</param>
+        public AlwaysReturnUnknownMethodInvoker(IUnknownValueFactory unknownValueFactory)
+        {
+            UnknownValueFactory = unknownValueFactory ?? throw new ArgumentNullException(nameof(unknownValueFactory));
+        }
+        
+        /// <summary>
+        /// Gets the factory that is responsible for constructing the unknown values. 
+        /// </summary>
+        public IUnknownValueFactory UnknownValueFactory
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public ICliValue Invoke(MethodDefinition method, IEnumerable<ICliValue> arguments)
+        {
+            var returnType = method.Signature.ReturnType;
+            return returnType.ElementType != ElementType.Void 
+                ? UnknownValueFactory.CreateUnknown(returnType) 
+                : null;
+        }
+    }
+}
