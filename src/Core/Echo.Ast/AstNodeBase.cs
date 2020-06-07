edb0dfd72b4852b4f508156249fdf5be4bc4a989
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 18:14:14 +0200

    Implement IGraph in AstNodeBase

--- src/Core/Echo.Ast/AstNodeBase.cs
+++ src/Core/Echo.Ast/AstNodeBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Echo.Core.Graphing;
 
 namespace Echo.Ast
@@ -6,21 +8,24 @@ namespace Echo.Ast
     /// <summary>
     /// Provides a contract for all nodes in an AST
     /// </summary>
-    public abstract class AstNodeBase : INode
+    public abstract class AstNodeBase : INode, IGraph
     {
         /// <summary>
-        /// A simple dictionary for storing annotations
+        /// Assigns the node with an id
         /// </summary>
-        public abstract IDictionary<string, object> UserData
+        /// <param name="id">The unique ID to assign to the node</param>
+        protected AstNodeBase(long id)
+        {
+            Id = id;
+        }
+        
+        /// <summary>
+        /// A simple dictionary for storing user annotations
+        /// </summary>
+        public IDictionary<string, object> UserData
         {
             get;
-        }
-
-        /// <summary>
-        /// Gets the children of the current AST node
-        /// </summary>
-        /// <returns>All children of the current AST node</returns>
-        public abstract IEnumerable<AstNodeBase> GetChildren();
+        } = new Dictionary<string, object>();
 
         /// <inheritdoc />
         public long Id
@@ -31,49 +36,89 @@ namespace Echo.Ast
         /// <inheritdoc />
         public int InDegree
         {
-            get;
+            get => throw new NotSupportedException();
         }
 
         /// <inheritdoc />
         public int OutDegree
         {
-            get;
+            get => GetChildren().Count();
         }
+
+        /// <summary>
+        /// Gets the children of the current AST node
+        /// </summary>
+        /// <returns>Immediate children of the current AST node</returns>
+        public abstract IEnumerable<AstNodeBase> GetChildren();
 
         /// <inheritdoc />
         public IEnumerable<IEdge> GetIncomingEdges()
         {
-            throw new System.NotImplementedException();
+            throw new NotSupportedException();
         }
 
         /// <inheritdoc />
         public IEnumerable<IEdge> GetOutgoingEdges()
         {
-            throw new System.NotImplementedException();
+            return GetChildren().Select(child => new Edge(this, child)).Cast<IEdge>();
         }
 
         /// <inheritdoc />
         public IEnumerable<INode> GetPredecessors()
         {
-            throw new System.NotImplementedException();
+            throw new NotSupportedException();
         }
 
         /// <inheritdoc />
         public IEnumerable<INode> GetSuccessors()
         {
-            throw new System.NotImplementedException();
+            return GetChildren();
         }
 
         /// <inheritdoc />
         public bool HasPredecessor(INode node)
         {
-            throw new System.NotImplementedException();
+            throw new NotSupportedException();
         }
 
         /// <inheritdoc />
         public bool HasSuccessor(INode node)
         {
-            throw new System.NotImplementedException();
+            return GetChildren().Contains(node);
+        }
+
+        /// <inheritdoc />
+        public INode GetNodeById(long id)
+        {
+            return GetNodes().Single(node => node.Id == id);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<INode> GetNodes()
+        {
+            return GetAllChildren();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<ISubGraph> GetSubGraphs()
+        {
+            return Array.Empty<ISubGraph>();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<IEdge> GetEdges()
+        {
+            return GetAllChildren().Select(child => child.GetOutgoingEdges()).SelectMany(e => e);
+        }
+
+        private IEnumerable<AstNodeBase> GetAllChildren()
+        {
+            var children = new List<AstNodeBase>();
+            
+            foreach (var child in GetChildren())
+                children.AddRange(child.GetAllChildren());
+
+            return children;
         }
     }
 }
