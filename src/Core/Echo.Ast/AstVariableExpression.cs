1eaf85301cfc12bea0bb82358aa1185c823a4880
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 18:14:46 +0200

    Add variable expression

---
+++ src/Core/Echo.Ast/AstVariableExpression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Echo.Core.Code;
+
+namespace Echo.Ast
+{
+    /// <summary>
+    /// Represents a variable in an AST
+    /// </summary>
+    public sealed class AstVariableExpression : AstExpressionBase
+    {
+        /// <summary>
+        /// Creates a new variable expression
+        /// </summary>
+        /// <param name="id"><inheritdoc cref="AstNodeBase(long)"/></param>
+        /// <param name="variable">The variable</param>
+        public AstVariableExpression(long id, IVariable variable)
+            : base(id)
+        {
+            Variable = variable;
+        }
+
+        /// <summary>
+        /// The variable that the node represents
+        /// </summary>
+        public IVariable Variable
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<AstNodeBase> GetChildren()
+        {
+            return Array.Empty<AstNodeBase>();
+        }
+    }
+}
