d5600540ea4c215dcb616504f8a926c9542b5b12
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 19:32:26 +0200

    Create root AST class

---
+++ src/Core/Echo.Ast/AbstractSyntaxTree.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Echo.Ast
+{
+    /// <summary>
+    /// Represents an AST
+    /// </summary>
+    /// <remarks>This is not called 'Ast', because of the namespace</remarks>
+    public sealed class AbstractSyntaxTree : AstNodeBase
+    {
+        /// <summary>
+        /// Creates a new AST
+        /// </summary>
+        public AbstractSyntaxTree()
+            : base(0) { }
+
+        /// <summary>
+        /// The tree's statements
+        /// </summary>
+        public IList<AstStatementBase> Statements
+        {
+            get;
+        } = new List<AstStatementBase>();
+
+        /// <inheritdoc />
+        public override IEnumerable<AstNodeBase> GetChildren()
+        {
+            return Statements;
+        }
+    }
+}
