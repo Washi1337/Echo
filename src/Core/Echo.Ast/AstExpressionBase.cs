e4f8fae50a678b6ffff30fe793736e7ff6b75fee
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 17:34:48 +0200

    Add skeleton for AST nodes

---
+++ src/Core/Echo.Ast/AstExpressionBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Echo.Ast
+{
+    /// <summary>
+    /// A base type for expression nodes in an AST
+    /// </summary>
+    public abstract class AstExpressionBase : AstNodeBase
+    {
+        /// <inheritdoc />
+        public override IDictionary<string, object> UserData
+        {
+            get;
+        } = new Dictionary<string, object>();
+    }
+}
---
+++ src/Core/Echo.Ast/AstNodeBase.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Echo.Core.Graphing;
+
+namespace Echo.Ast
+{
+    /// <summary>
+    /// Provides a contract for all nodes in an AST
+    /// </summary>
+    public abstract class AstNodeBase : INode
+    {
+        /// <summary>
+        /// A simple dictionary for storing annotations
+        /// </summary>
+        public abstract IDictionary<string, object> UserData
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the children of the current AST node
+        /// </summary>
+        /// <returns>All children of the current AST node</returns>
+        public abstract IEnumerable<AstNodeBase> GetChildren();
+
+        /// <inheritdoc />
+        public long Id
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public int InDegree
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public int OutDegree
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<IEdge> GetIncomingEdges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<IEdge> GetOutgoingEdges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<INode> GetPredecessors()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<INode> GetSuccessors()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool HasPredecessor(INode node)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public bool HasSuccessor(INode node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
