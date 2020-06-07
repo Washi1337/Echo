8a20a3f6561ed65bdd83dd6a5b713495f5b57df7
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 22:35:32 +0200

    Create placeholder variable for stack slots

---
+++ src/Core/Echo.Ast/StackVariable.cs
@@ -0,0 +1,25 @@
+﻿using Echo.Core.Code;
+
+namespace Echo.Ast
+{
+    /// <summary>
+    /// Placeholder variable for replacing stack slots
+    /// </summary>
+    public sealed class StackVariable : IVariable
+    {
+        /// <summary>
+        /// Creates a new placeholder variable for a stack slot
+        /// </summary>
+        /// <param name="name">The name of the variable</param>
+        public StackVariable(string name)
+        {
+            Name = name;
+        }
+
+        /// <inheritdoc />
+        public string Name
+        {
+            get;
+        }
+    }
+}
