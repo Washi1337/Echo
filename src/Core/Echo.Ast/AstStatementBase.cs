0edb9099d12178f8036c8f3a9b79c23a21f584ca
zsr2531 <warhaamer1337@gmail.com>
2020-06-07 18:14:58 +0200

    Create a contract for statements

---
+++ src/Core/Echo.Ast/AstStatementBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Echo.Ast
+{
+    /// <summary>
+    /// A base type for statement nodes in an AST
+    /// </summary>
+    public abstract class AstStatementBase : AstNodeBase
+    {
+        /// <inheritdoc />
+        protected AstStatementBase(long id)
+            : base(id) { }
+    }
+}
