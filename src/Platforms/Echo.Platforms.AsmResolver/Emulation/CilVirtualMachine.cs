4e1a6c2e53df5036ab543d6c34f1cc63afd79dd0
Washi <washi1337@protonmail.com>
2020-06-18 22:18:18 +0200

    Add ICilRuntimeEnvironment.StaticFieldFactory.

--- src/Platforms/Echo.Platforms.AsmResolver/Emulation/CilVirtualMachine.cs
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/CilVirtualMachine.cs
@@ -62,6 +62,7 @@ namespace Echo.Platforms.AsmResolver.Emulation
             CliMarshaller = new DefaultCliMarshaller(this);
             MemoryAllocator = new DefaultMemoryAllocator(module, is32Bit);
             MethodInvoker = new ReturnUnknownMethodInvoker(new UnknownValueFactory(is32Bit));
+            StaticFieldFactory = new StaticFieldFactory();
             
             _services[typeof(ICilRuntimeEnvironment)] = this;
         }
@@ -122,6 +123,12 @@ namespace Echo.Platforms.AsmResolver.Emulation
         {
             get;
             set;
+        }
+
+        /// <inheritdoc />
+        public StaticFieldFactory StaticFieldFactory
+        {
+            get;
         }
 
         /// <summary>
