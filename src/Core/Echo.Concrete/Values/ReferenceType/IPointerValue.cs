db8a990def8b36f09c29c8eae844fb8f3b0ed15a
Washi <washi1337@protonmail.com>
2020-06-26 22:13:26 +0200

    Extract IPointerValue.

---
+++ src/Core/Echo.Concrete/Values/ReferenceType/IPointerValue.cs
@@ -0,0 +1,166 @@
+using System;
+using Echo.Concrete.Values.ValueType;
+
+namespace Echo.Concrete.Values.ReferenceType
+{
+    /// <summary>
+    /// Represents a pointer value.
+    /// </summary>
+    public interface IPointerValue : IConcreteValue
+    {
+        /// <summary>
+        /// Reads raw contents of the memory block. 
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <param name="memoryBuffer">The memory buffer to copy data to.</param>
+        /// <remarks>
+        /// This method has undefined behaviour if the memory contains unknown bits.
+        /// </remarks>
+        void ReadBytes(int offset, Span<byte> memoryBuffer);
+
+        /// <summary>
+        /// Reads raw contents of the memory block. 
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <param name="memoryBuffer">The memory buffer to copy data to.</param>
+        /// <param name="knownBitmaskBuffer">The buffer to copy the known bitmask to.</param>
+        void ReadBytes(int offset, Span<byte> memoryBuffer, Span<byte> knownBitmaskBuffer);
+
+        /// <summary>
+        /// Writes raw data to the memory block as fully known bytes.
+        /// </summary>
+        /// <param name="offset">The offset to start writing.</param>
+        /// <param name="data">The data to write.</param>
+        void WriteBytes(int offset, ReadOnlySpan<byte> data);
+
+        /// <summary>
+        /// Writes raw data to the memory block.
+        /// </summary>
+        /// <param name="offset">The offset to start writing.</param>
+        /// <param name="data">The data to write.</param>
+        /// <param name="knownBitMask">
+        /// The bitmask indicating the bits that are known within the data referenced by <paramref name="data"/>.
+        /// </param>
+        void WriteBytes(int offset, ReadOnlySpan<byte> data, ReadOnlySpan<byte> knownBitMask);
+
+        /// <summary>
+        /// Reads a single 8 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read integer.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        Integer8Value ReadInteger8(int offset);
+
+        /// <summary>
+        /// Reads a single 16 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read integer.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        Integer16Value ReadInteger16(int offset);
+
+        /// <summary>
+        /// Reads a single 32 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read integer.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        Integer32Value ReadInteger32(int offset);
+
+        /// <summary>
+        /// Reads a single 64 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read integer.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        Integer64Value ReadInteger64(int offset);
+
+        /// <summary>
+        /// Reads a single 32 bit floating point number at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        unsafe Float32Value ReadFloat32(int offset);
+
+        /// <summary>
+        /// Reads a single 64 bit floating point number at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start reading.</param>
+        /// <returns>The read number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        unsafe Float64Value ReadFloat64(int offset);
+
+        /// <summary>
+        /// Writes a single 8 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        void WriteInteger8(int offset, Integer8Value value);
+
+        /// <summary>
+        /// Writes a single 16 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        void WriteInteger16(int offset, Integer16Value value);
+
+        /// <summary>
+        /// Writes a single 32 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        void WriteInteger32(int offset, Integer32Value value);
+
+        /// <summary>
+        /// Writes a single 64 bit integer at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        void WriteInteger64(int offset, Integer64Value value);
+
+        /// <summary>
+        /// Writes a single 32 bit floating point number at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        unsafe void WriteFloat32(int offset, Float32Value value);
+
+        /// <summary>
+        /// Writes a single 64 bit floating point number at the provided offset.
+        /// </summary>
+        /// <param name="offset">The offset to start writing at.</param>
+        /// <param name="value">The value to write.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Occurs when the offset does not fall within the memory range.
+        /// </exception>
+        unsafe void WriteFloat64(int offset, Float64Value value);
+    }
+}
--- src/Core/Echo.Concrete/Values/ReferenceType/MemoryPointerValue.cs
+++ src/Core/Echo.Concrete/Values/ReferenceType/MemoryPointerValue.cs
@@ -9,7 +9,7 @@ namespace Echo.Concrete.Values.ReferenceType
     /// <summary>
     /// Represents a pointer to the beginning of a chunk of memory.
     /// </summary>
-    public class MemoryPointerValue : IConcreteValue
+    public class MemoryPointerValue : IPointerValue
     {
         private readonly Memory<byte> _memory;
         private readonly Memory<byte> _knownBitMask;
@@ -64,26 +64,14 @@ namespace Echo.Concrete.Values.ReferenceType
         /// <inheritdoc />
         public bool? IsNegative => false;
 
-        /// <summary>
-        /// Reads raw contents of the memory block. 
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <param name="memoryBuffer">The memory buffer to copy data to.</param>
-        /// <remarks>
-        /// This method has undefined behaviour if the memory contains unknown bits.
-        /// </remarks>
+        /// <inheritdoc />
         public void ReadBytes(int offset, Span<byte> memoryBuffer)
         {
             var slicedMemory = _memory.Span.Slice(offset);
             slicedMemory.CopyTo(memoryBuffer);
         }
 
-        /// <summary>
-        /// Reads raw contents of the memory block. 
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <param name="memoryBuffer">The memory buffer to copy data to.</param>
-        /// <param name="knownBitmaskBuffer">The buffer to copy the known bitmask to.</param>
+        /// <inheritdoc />
         public void ReadBytes(int offset, Span<byte> memoryBuffer, Span<byte> knownBitmaskBuffer)
         {
             var slicedMemory = _memory.Span.Slice(offset);
@@ -93,11 +81,7 @@ namespace Echo.Concrete.Values.ReferenceType
             slicedBitMask.CopyTo(knownBitmaskBuffer);
         }
         
-        /// <summary>
-        /// Writes raw data to the memory block as fully known bytes.
-        /// </summary>
-        /// <param name="offset">The offset to start writing.</param>
-        /// <param name="data">The data to write.</param>
+        /// <inheritdoc />
         public void WriteBytes(int offset, ReadOnlySpan<byte> data)
         {
             var slicedMemory = _memory.Span.Slice(offset);
@@ -107,14 +91,7 @@ namespace Echo.Concrete.Values.ReferenceType
             slicedBitMask.Fill(0xFF);
         }
 
-        /// <summary>
-        /// Writes raw data to the memory block.
-        /// </summary>
-        /// <param name="offset">The offset to start writing.</param>
-        /// <param name="data">The data to write.</param>
-        /// <param name="knownBitMask">
-        /// The bitmask indicating the bits that are known within the data referenced by <paramref name="data"/>.
-        /// </param>
+        /// <inheritdoc />
         public void WriteBytes(int offset, ReadOnlySpan<byte> data, ReadOnlySpan<byte> knownBitMask)
         {
             var slicedMemory = _memory.Span.Slice(offset);
@@ -124,28 +101,14 @@ namespace Echo.Concrete.Values.ReferenceType
             knownBitMask.CopyTo(slicedBitMask);
         }
         
-        /// <summary>
-        /// Reads a single 8 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read integer.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public Integer8Value ReadInteger8(int offset)
         {
             AssertOffsetValidity(offset, sizeof(byte));
             return new Integer8Value(_memory.Span[offset], _knownBitMask.Span[offset]);
         }
 
-        /// <summary>
-        /// Reads a single 16 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read integer.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public Integer16Value ReadInteger16(int offset)
         {
             AssertOffsetValidity(offset, sizeof(ushort));
@@ -158,14 +121,7 @@ namespace Echo.Concrete.Values.ReferenceType
                 BinaryPrimitives.ReadUInt16LittleEndian(knownBitsSpan));
         }
 
-        /// <summary>
-        /// Reads a single 32 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read integer.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public Integer32Value ReadInteger32(int offset)
         {
             AssertOffsetValidity(offset, sizeof(uint));
@@ -178,14 +134,7 @@ namespace Echo.Concrete.Values.ReferenceType
                 BinaryPrimitives.ReadUInt32LittleEndian(knownBitsSpan));
         }
 
-        /// <summary>
-        /// Reads a single 64 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read integer.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public Integer64Value ReadInteger64(int offset)
         {
             AssertOffsetValidity(offset, sizeof(ulong));
@@ -198,14 +147,7 @@ namespace Echo.Concrete.Values.ReferenceType
                 BinaryPrimitives.ReadUInt64LittleEndian(knownBitsSpan));
         }
 
-        /// <summary>
-        /// Reads a single 32 bit floating point number at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read number.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public unsafe Float32Value ReadFloat32(int offset)
         {
             AssertOffsetValidity(offset, sizeof(float));
@@ -217,14 +159,7 @@ namespace Echo.Concrete.Values.ReferenceType
             return new Float32Value(*(float*) ((byte*) handle.Pointer + offset));
         }
 
-        /// <summary>
-        /// Reads a single 64 bit floating point number at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start reading.</param>
-        /// <returns>The read number.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public unsafe Float64Value ReadFloat64(int offset)
         {
             AssertOffsetValidity(offset, sizeof(double));
@@ -236,14 +171,7 @@ namespace Echo.Concrete.Values.ReferenceType
             return new Float64Value(*(double*) ((byte*) handle.Pointer + offset));
         }
 
-        /// <summary>
-        /// Writes a single 8 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public void WriteInteger8(int offset, Integer8Value value)
         {
             AssertOffsetValidity(offset, sizeof(byte));
@@ -252,14 +180,7 @@ namespace Echo.Concrete.Values.ReferenceType
             _knownBitMask.Span[offset] = value.Mask;
         }
 
-        /// <summary>
-        /// Writes a single 16 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public void WriteInteger16(int offset, Integer16Value value)
         {
             AssertOffsetValidity(offset, sizeof(ushort));
@@ -268,14 +189,7 @@ namespace Echo.Concrete.Values.ReferenceType
             BinaryPrimitives.WriteUInt16LittleEndian(_knownBitMask.Span.Slice(offset, 2), value.Mask);
         }
 
-        /// <summary>
-        /// Writes a single 32 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public void WriteInteger32(int offset, Integer32Value value)
         {
             AssertOffsetValidity(offset, sizeof(uint));
@@ -284,14 +198,7 @@ namespace Echo.Concrete.Values.ReferenceType
             BinaryPrimitives.WriteUInt32LittleEndian(_knownBitMask.Span.Slice(offset, 4), value.Mask);
         }
 
-        /// <summary>
-        /// Writes a single 64 bit integer at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public void WriteInteger64(int offset, Integer64Value value)
         {
             AssertOffsetValidity(offset, sizeof(ulong));
@@ -300,14 +207,7 @@ namespace Echo.Concrete.Values.ReferenceType
             BinaryPrimitives.WriteUInt64LittleEndian(_knownBitMask.Span.Slice(offset, 8), value.Mask);
         }
 
-        /// <summary>
-        /// Writes a single 32 bit floating point number at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public unsafe void WriteFloat32(int offset, Float32Value value)
         {
             AssertOffsetValidity(offset, sizeof(float));
@@ -320,14 +220,7 @@ namespace Echo.Concrete.Values.ReferenceType
             _knownBitMask.Span.Slice(offset, 4).Fill(0xFF);
         }
 
-        /// <summary>
-        /// Writes a single 64 bit floating point number at the provided offset.
-        /// </summary>
-        /// <param name="offset">The offset to start writing at.</param>
-        /// <param name="value">The value to write.</param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        /// Occurs when the offset does not fall within the memory range.
-        /// </exception>
+        /// <inheritdoc />
         public unsafe void WriteFloat64(int offset, Float64Value value)
         {
             AssertOffsetValidity(offset, sizeof(double));
