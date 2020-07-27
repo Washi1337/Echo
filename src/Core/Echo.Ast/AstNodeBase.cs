ecad2b84de4a26aafba70e8a3f1de9c5b99577de
zsr2531 <warhaamer1337@gmail.com>
2020-07-27 22:05:49 +0200

    Use Ast ISA decorator in Ast node and CompilationUnit

--- src/Core/Echo.Ast/AstNodeBase.cs
+++ src/Core/Echo.Ast/AstNodeBase.cs
@@ -8,15 +8,25 @@ namespace Echo.Ast
     /// <summary>
     /// Provides a base contract for all Ast nodes
     /// </summary>
-    public abstract class AstNodeBase : INode
+    public abstract class AstNodeBase<TInstruction> : INode
     {
         /// <summary>
         /// Assigns the unique ID to the node
         /// </summary>
         /// <param name="id">The unique identifier</param>
-        protected AstNodeBase(long id)
+        /// <param name="content">Instruction to represent in the AST node</param>
+        protected AstNodeBase(long id, TInstruction content)
         {
             Id = id;
+            Content = content;
+        }
+
+        /// <summary>
+        /// Gets the <typeparamref name="TInstruction"/> represented by this AST node
+        /// </summary>
+        public TInstruction Content
+        {
+            get;
         }
 
         /// <inheritdoc />
@@ -32,10 +42,10 @@ namespace Echo.Ast
         public int OutDegree => GetOutgoingEdges().Count();
 
         /// <summary>
-        /// Gets the children of the current <see cref="AstNodeBase"/>
+        /// Gets the children of the current <see cref="AstNodeBase{TInstruction}"/>
         /// </summary>
         /// <returns>The children</returns>
-        public abstract IEnumerable<AstNodeBase> GetChildren();
+        public abstract IEnumerable<AstNodeBase<TInstruction>> GetChildren();
 
         /// <inheritdoc />
         public IEnumerable<IEdge> GetIncomingEdges() => throw new NotSupportedException();
--- src/Core/Echo.Ast/CompilationUnit.cs
+++ src/Core/Echo.Ast/CompilationUnit.cs
@@ -1,30 +1,17 @@
-using System.Collections.Generic;
 using Echo.ControlFlow;
+using Echo.Core.Code;
 
 namespace Echo.Ast
 {
     /// <summary>
     /// Represents the root of the syntax tree
     /// </summary>
-    public class CompilationUnit : ControlFlowGraph<AstNodeBase>
+    public class CompilationUnit<TInstruction> : ControlFlowGraph<AstNodeBase<TInstruction>>
     {
         /// <summary>
-        /// Creates a new instance of <see cref="CompilationUnit"/>
+        /// Creates a new instance of <see cref="CompilationUnit{TInstruction}"/>
         /// </summary>
-        public CompilationUnit(long id)
-            : base(id) { }
-
-        /// <summary>
-        /// All the children of the <see cref="CompilationUnit"/>
-        /// </summary>
-        public ICollection<AstNodeBase> Children
-        {
-            get;
-        } = new List<AstNodeBase>();
-
-        public override IEnumerable<AstNodeBase> GetChildren()
-        {
-            return Children;
-        }
+        public CompilationUnit(IInstructionSetArchitecture<TInstruction> isa)
+            : base(new AstInstructionSetArchitectureDecorator<TInstruction>(isa)) { }
     }
 }
