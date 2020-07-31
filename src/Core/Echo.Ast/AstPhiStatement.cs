1161b4a0ab8f4fe12f6d90a1da5eb1ae3eb6bf9c
zsr2531 <warhaamer1337@gmail.com>
2020-07-31 23:23:55 +0200

    Create phi node

---
+++ src/Core/Echo.Ast/AstPhiStatement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Echo.Core.Code;
+
+namespace Echo.Ast
+{
+    public sealed class AstPhiStatement<TInstruction> : AstStatementBase<TInstruction>
+    {
+        public AstPhiStatement(long id, IEnumerable<AstVariableExpression<TInstruction>> sources, IVariable target) : base(id)
+        {
+            Sources = sources;
+            Target = target;
+        }
+
+        public AstVariableExpression<TInstruction> Sources
+        {
+            get;
+        }
+
+        public IVariable Target
+        {
+            get;
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<AstNodeBase<TInstruction>> GetChildren()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override void Accept<TState>(IAstNodeVisitor<TInstruction, TState> visitor, TState state)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override TOut Accept<TState, TOut>(IAstNodeVisitor<TInstruction, TState, TOut> visitor, TState state)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
