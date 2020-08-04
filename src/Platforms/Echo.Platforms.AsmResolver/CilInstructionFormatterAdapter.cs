2d487337796d5485774e079573b1c4d3bc48bfbe
zsr2531 <warhaamer1337@gmail.com>
2020-08-04 16:12:12 +0200

    Use existing CilInstruction formatter from AsmResolver in form of an adapter

---
+++ src/Platforms/Echo.Platforms.AsmResolver/CilInstructionFormatterAdapter.cs
@@ -0,0 +1,21 @@
+﻿using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.ControlFlow.Serialization.Dot;
+
+namespace Echo.Platforms.AsmResolver
+{
+    /// <summary>
+    /// Provides a custom formatter for <see cref="CilInstruction"/>s.
+    /// </summary>
+    public class CilInstructionFormatterAdapter : CilInstructionFormatter, IInstructionFormatter<CilInstruction>
+    {
+        /// <inheritdoc />
+        public string Format(in CilInstruction instruction)
+        {
+            string minimal = $"{FormatOpCode(instruction.OpCode)}";
+            return instruction.Operand is null
+                ? minimal
+                : $"{minimal} {FormatOperand(instruction.OpCode.OperandType, instruction.Operand)}";
+        }
+    }
+}
