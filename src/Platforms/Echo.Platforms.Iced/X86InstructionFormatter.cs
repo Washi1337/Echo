af8c7e5790b91869ec35cf0ac285c5dc8df05b12
zsr2531 <warhaamer1337@gmail.com>
2020-08-04 15:24:22 +0200

    Add instruction formatters for existing backends

---
+++ src/Platforms/Echo.Platforms.Iced/X86InstructionFormatter.cs
@@ -0,0 +1,14 @@
+﻿using Echo.ControlFlow.Serialization.Dot;
+using Iced.Intel;
+
+namespace Echo.Platforms.Iced
+{
+    /// <summary>
+    /// Provides a custom formatter for <see cref="Instruction"/>s.
+    /// </summary>
+    public class CilInstructionFormatter : IInstructionFormatter<Instruction>
+    {
+        /// <inheritdoc />
+        public string Format(in Instruction instruction) => $"{instruction}";
+    }
+}
