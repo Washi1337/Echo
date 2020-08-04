95dabd6027d221f515648f9c850c3f0cf013c454
zsr2531 <warhaamer1337@gmail.com>
2020-08-04 15:15:47 +0200

    Create default formatter

---
+++ src/Core/Echo.ControlFlow/Serialization/Dot/DefaultInstructionFormatter.cs
@@ -0,0 +1,10 @@
+﻿namespace Echo.ControlFlow.Serialization.Dot
+{
+    internal sealed class DefaultInstructionFormatter<TInstruction> : IInstructionFormatter<TInstruction>
+    {
+        public string Format(in TInstruction instruction)
+        {
+            return instruction.ToString();
+        }
+    }
+}
