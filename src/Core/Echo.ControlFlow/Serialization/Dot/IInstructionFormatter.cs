0a7711fecbab0447d24d9e7f40aba30c6f5026fb
zsr2531 <warhaamer1337@gmail.com>
2020-08-04 15:15:41 +0200

    Fix missing periods in xmldoc

--- src/Core/Echo.ControlFlow/Serialization/Dot/IInstructionFormatter.cs
+++ src/Core/Echo.ControlFlow/Serialization/Dot/IInstructionFormatter.cs
@@ -1,16 +1,16 @@
 ﻿namespace Echo.ControlFlow.Serialization.Dot
 {
     /// <summary>
-    /// Allows the user to format instructions in the <see cref="ControlFlowEdgeAdorner{TInstruction}"/>
+    /// Allows the user to format instructions in the <see cref="ControlFlowEdgeAdorner{TInstruction}"/>.
     /// </summary>
     /// <typeparam name="TInstruction">The type of instructions the nodes contain.</typeparam>
     public interface IInstructionFormatter<TInstruction>
     {
         /// <summary>
-        /// Formats a given <typeparamref name="TInstruction"/>
+        /// Formats a given <typeparamref name="TInstruction"/>.
         /// </summary>
-        /// <param name="instruction">The <typeparamref name="TInstruction"/> to format</param>
-        /// <returns>The formatted <paramref name="instruction"/></returns>
+        /// <param name="instruction">The <typeparamref name="TInstruction"/> to format.</param>
+        /// <returns>The formatted <paramref name="instruction"/>.</returns>
         string Format(in TInstruction instruction);
     }
 }
