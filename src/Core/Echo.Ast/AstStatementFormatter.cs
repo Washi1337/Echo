7166ac37f867007fd5abc6665d6954d70d1f51c6
zsr2531 <warhaamer1337@gmail.com>
2020-08-04 19:27:52 +0200

    Create AST statement formatter

---
+++ src/Core/Echo.Ast/AstStatementFormatter.cs
@@ -0,0 +1,18 @@
+﻿using Echo.ControlFlow.Serialization.Dot;
+
+namespace Echo.Ast
+{
+    public class AstStatementFormatter<TInstruction> : IInstructionFormatter<AstStatementBase<TInstruction>>
+    {
+        private readonly IInstructionFormatter<TInstruction> _instructionFormatter;
+        
+        public AstStatementFormatter(IInstructionFormatter<TInstruction> instructionFormatter)
+        {
+            _instructionFormatter = instructionFormatter;
+        }
+
+        /// <inheritdoc />
+        public string Format(in AstStatementBase<TInstruction> instruction) =>
+            instruction.Format(_instructionFormatter);
+    }
+}
