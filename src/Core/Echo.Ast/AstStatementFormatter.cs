49336ec602a4f0b9a93504c70fe755b381a23565
zsr2531 <warhaamer1337@gmail.com>
2020-08-07 21:35:14 +0200

    Missing comment bad

--- src/Core/Echo.Ast/AstStatementFormatter.cs
+++ src/Core/Echo.Ast/AstStatementFormatter.cs
@@ -2,14 +2,21 @@
 
 namespace Echo.Ast
 {
+    /// <summary>
+    /// Formats the Ast so it looks nicer in <see cref="ControlFlowNodeAdorner{TInstruction}"/>
+    /// </summary>
     public class AstStatementFormatter<TInstruction> : IInstructionFormatter<AstStatementBase<TInstruction>>
     {
         private readonly IInstructionFormatter<TInstruction> _instructionFormatter;
         
-        public AstStatementFormatter(IInstructionFormatter<TInstruction> instructionFormatter)
-        {
+        /// <summary>
+        /// Creates a new instance of <see cref="AstStatementFormatter{TInstruction}"/> with the specified
+        /// <see cref="IInstructionFormatter{TInstruction}"/>
+        /// </summary>
+        /// <param name="instructionFormatter">The <see cref="IInstructionFormatter{TInstruction}"/> used to
+        /// format <see cref="AstInstructionExpression{TInstruction}"/>s with</param>
+        public AstStatementFormatter(IInstructionFormatter<TInstruction> instructionFormatter) =>
             _instructionFormatter = instructionFormatter;
-        }
 
         /// <inheritdoc />
         public string Format(in AstStatementBase<TInstruction> instruction) =>
