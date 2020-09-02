4270702a7c21b3c7c4daa8d424f02f2c8feefe6b
Washi <washi1337@protonmail.com>
2020-09-02 22:20:59 +0200

    Add OffsetNodeIdentifier

---
+++ src/Core/Echo.ControlFlow/Serialization/Dot/OffsetNodeIdentifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Echo.Core.Graphing;
+using Echo.Core.Graphing.Serialization.Dot;
+
+namespace Echo.ControlFlow.Serialization.Dot
+{
+    /// <summary>
+    /// Provides an implementation of the <see cref="INodeIdentifier"/> interface, that returns the offset of the basic
+    /// block as unique identifiers.
+    /// </summary>
+    /// <typeparam name="TInstruction">The type of instructions stored in the basic block.</typeparam>
+    public class OffsetNodeIdentifier<TInstruction> : INodeIdentifier
+    {
+        /// <summary>
+        /// Provides a default instance of the <see cref="OffsetNodeIdentifier{TInstruction}"/> class. 
+        /// </summary>
+        public static OffsetNodeIdentifier<TInstruction> Instance
+        {
+            get;
+        } = new OffsetNodeIdentifier<TInstruction>();
+        
+        /// <inheritdoc />
+        public long GetIdentifier(INode node) => node is ControlFlowNode<TInstruction> controlFlowNode
+            ? controlFlowNode.Offset
+            : throw new ArgumentOutOfRangeException(nameof(node));
+    }
+}
