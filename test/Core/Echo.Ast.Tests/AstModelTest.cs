8d028117c9ee394cc672ba1d4877f43e571bd087
zsr2531 <warhaamer1337@gmail.com>
2020-10-05 21:57:37 +0200

    Add tests for remaining Ast models

--- test/Core/Echo.Ast.Tests/AstModelTest.cs
+++ test/Core/Echo.Ast.Tests/AstModelTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Echo.ControlFlow;
 using Echo.ControlFlow.Construction;
 using Echo.ControlFlow.Construction.Symbolic;
@@ -150,16 +151,79 @@ namespace Echo.Ast.Tests
 
             var stmt = Assert.IsType<ExpressionStatement<DummyInstruction>>(block[1]);
             var expr = Assert.IsType<InstructionExpression<DummyInstruction>>(stmt.Expression);
-            
+
             Assert.Single(expr.Arguments);
             var original = Assert.IsType<VariableExpression<DummyInstruction>>(expr.Arguments[0]);
             var newOne = new VariableExpression<DummyInstruction>(new DummyVariable("dummy"));
 
             expr.WithArguments(original, newOne);
-            
+
             Assert.Equal(2, expr.Arguments.Count);
             Assert.Equal(original, expr.Arguments[0]);
             Assert.Equal(newOne, expr.Arguments[1]);
+            Assert.Equal(expr, newOne.Parent);
+        }
+
+        [Fact]
+        public void ReplacingTargetVariableInPhiStatement()
+        {
+            var target1 = new DummyVariable("old");
+            var target2 = new DummyVariable("new");
+            var sources = new[]
+            {
+                new DummyVariable("source1"),
+                new DummyVariable("source2"),
+                new DummyVariable("source3"),
+                new DummyVariable("source4")
+            };
+            var phi = new PhiStatement<DummyInstruction>(
+                target1, sources.Select(s => new VariableExpression<DummyInstruction>(s)));
+
+            Assert.Equal(target2, phi.WithTarget(target2).Target);
+        }
+
+        [Fact]
+        public void ReplacingSourcesInPhiStatement()
+        {
+            var target = new DummyVariable("target");
+            var sources1 = new[]
+            {
+                new DummyVariable("source1"),
+                new DummyVariable("source2"),
+                new DummyVariable("source3"),
+                new DummyVariable("source4")
+            };
+            var sources2 = new[]
+            {
+                new DummyVariable("newSource1"),
+                new DummyVariable("newSource2"),
+                new DummyVariable("newSource3"),
+                new DummyVariable("newSource4"),
+                new DummyVariable("newSource5")
+            };
+            var phi = new PhiStatement<DummyInstruction>(
+                target, sources1.Select(s => new VariableExpression<DummyInstruction>(s)));
+
+            var sourceExpr = sources2.Select(s => new VariableExpression<DummyInstruction>(s)).ToList();
+            phi.WithSources(sourceExpr);
+            
+            Assert.Equal(5, phi.Sources.Count);
+            Assert.All(phi.Sources, s =>
+            {
+                Assert.Equal(phi, s.Parent);
+                Assert.Equal(sources2[sourceExpr.IndexOf(s)], s.Variable);
+            });
+        }
+
+        [Fact]
+        public void ReplacingVariableInVariableExpression()
+        {
+            var var1 = new DummyVariable("old");
+            var var2 = new DummyVariable("new");
+            
+            var expr = new VariableExpression<DummyInstruction>(var1);
+            
+            Assert.Equal(var2, expr.WithVariable(var2).Variable);
         }
     }
 }
