cde5b6f59bd8dfe9950ddab8f84695558a91dbe2
Washi <washi1337@protonmail.com>
2021-02-21 19:59:41 +0100

    Add failing tests with handlerend = null/endlabel.

---
+++ test/Platforms/Echo.Platforms.AsmResolver.Tests/StaticFlowGraphTest.cs
@@ -0,0 +1,57 @@
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.DotNet.Signatures;
+using AsmResolver.PE.DotNet.Cil;
+using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
+using Echo.ControlFlow.Regions;
+using Xunit;
+
+namespace Echo.Platforms.AsmResolver.Tests
+{
+    public class StaticFlowGraphTest
+    {
+        [Fact]
+        public void ExceptionHandlerWithHandlerEndOutsideOfMethod()
+        {
+            // https://github.com/Washi1337/Echo/issues/101
+
+            // Set up test case.
+            var module = new ModuleDefinition("Module");
+            var method = new MethodDefinition("MyMethod", MethodAttributes.Static,
+                MethodSignature.CreateStatic(module.CorLibTypeFactory.Void));
+            var body = method.CilMethodBody = new CilMethodBody(method);
+            var instructions = body.Instructions;
+
+            var start = new CilInstructionLabel();
+            var tryStart = new CilInstructionLabel();
+            var handlerStart = new CilInstructionLabel();
+
+            start.Instruction = instructions.Add(CilOpCodes.Nop);
+            tryStart.Instruction = instructions.Add(CilOpCodes.Nop);
+            instructions.Add(CilOpCodes.Leave_S, start);
+            handlerStart.Instruction = instructions.Add(CilOpCodes.Pop);
+            instructions.Add(CilOpCodes.Leave_S, start);
+
+            body.ExceptionHandlers.Add(new CilExceptionHandler
+            {
+                ExceptionType = module.CorLibTypeFactory.Object.ToTypeDefOrRef(),
+                TryStart = tryStart,
+                TryEnd = handlerStart,
+                HandlerStart = handlerStart,
+                HandlerEnd = instructions.EndLabel // Use end label.
+            });
+
+            instructions.CalculateOffsets();
+
+            // Construct CFG.
+            var cfg = body.ConstructStaticFlowGraph();
+            
+            // Verify handler exists.
+            Assert.NotNull(cfg);
+            Assert.Equal(3, cfg.Nodes.Count);
+            var region = Assert.Single(cfg.Regions);
+            var eh = Assert.IsAssignableFrom<ExceptionHandlerRegion<CilInstruction>>(region);
+            Assert.Single(eh.Handlers);
+        }
+    }
+}
