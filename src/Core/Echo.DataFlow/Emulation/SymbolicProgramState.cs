83269a8336da9b78aeedd697fc2a20c9c40a75e5
Washi <washi1337@protonmail.com>
2021-03-24 19:08:55 +0100

    Add missing xmldoc.

--- src/Core/Echo.DataFlow/Emulation/SymbolicProgramState.cs
+++ src/Core/Echo.DataFlow/Emulation/SymbolicProgramState.cs
@@ -6,13 +6,24 @@ using Echo.Core.Emulation;
 
 namespace Echo.DataFlow.Emulation
 {
+    /// <summary>
+    /// Represents an immutable snapshot of a program state that is fully symbolic.  
+    /// </summary>
+    /// <typeparam name="T">The type of instructions.</typeparam>
     public readonly struct SymbolicProgramState<T>
     {
+        /// <summary>
+        /// Gets an empty program state.
+        /// </summary>
         public static SymbolicProgramState<T> Empty
         {
             get;
         } = new(0);
         
+        /// <summary>
+        /// Creates a new empty program state, initialized at the provided program counter.
+        /// </summary>
+        /// <param name="programCounter">The initial program counter.</param>
         public SymbolicProgramState(long programCounter)
         {
             ProgramCounter = programCounter;
@@ -20,6 +31,11 @@ namespace Echo.DataFlow.Emulation
             Variables = ImmutableDictionary<IVariable, SymbolicValue<T>>.Empty;
         }
 
+        /// <summary>
+        /// Creates a new empty program state, initialized at the provided program counter.
+        /// </summary>
+        /// <param name="programCounter">The initial program counter.</param>
+        /// <param name="stack">The initial stack state.</param>
         public SymbolicProgramState(
             long programCounter, 
             ImmutableStack<SymbolicValue<T>> stack)
@@ -29,6 +45,11 @@ namespace Echo.DataFlow.Emulation
             Variables = ImmutableDictionary<IVariable, SymbolicValue<T>>.Empty;
         }
 
+        /// <summary>
+        /// Creates a new empty program state, initialized at the provided program counter.
+        /// </summary>
+        /// <param name="programCounter">The initial program counter.</param>
+        /// <param name="variables">The initial state of the variables.</param>
         public SymbolicProgramState(
             long programCounter, 
             ImmutableDictionary<IVariable, SymbolicValue<T>> variables)
@@ -38,6 +59,12 @@ namespace Echo.DataFlow.Emulation
             Variables = variables;
         }
         
+        /// <summary>
+        /// Creates a new empty program state, initialized at the provided program counter.
+        /// </summary>
+        /// <param name="programCounter">The initial program counter.</param>
+        /// <param name="stack">The initial stack state.</param>
+        /// <param name="variables">The initial state of the variables.</param>
         public SymbolicProgramState(
             long programCounter, 
             ImmutableStack<SymbolicValue<T>> stack,
@@ -48,36 +75,83 @@ namespace Echo.DataFlow.Emulation
             Variables = variables;
         }
 
+        /// <summary>
+        /// Gets the current value of the program counter that points to the instruction to be executed next.
+        /// </summary>
         public long ProgramCounter
         {
             get;
         }
-        
+
+        /// <summary>
+        /// Gets the current stack state of the program.
+        /// </summary>
         public ImmutableStack<SymbolicValue<T>> Stack
         {
             get;
         }
-
+        
+        /// <summary>
+        /// Gets the current variable state of the program.
+        /// </summary>
         public ImmutableDictionary<IVariable, SymbolicValue<T>> Variables
         {
             get;
         }
 
+        /// <summary>
+        /// Copies the current state and moves the program counter of the copy to the provided address.
+        /// </summary>
+        /// <param name="programCounter">The new program counter.</param>
+        /// <returns>The new program state.</returns>
         public SymbolicProgramState<T> WithProgramCounter(long programCounter) => 
             new(programCounter, Stack, Variables);
         
+        /// <summary>
+        /// Copies the current state and replaces the stack state with a new one.
+        /// </summary>
+        /// <param name="stack">The new stack state.</param>
+        /// <returns>The new program state.</returns>
         public SymbolicProgramState<T> WithStack(ImmutableStack<SymbolicValue<T>> stack) => 
             new(ProgramCounter, stack, Variables);
         
+        /// <summary>
+        /// Copies the current state and replaces the variables state with a new one.
+        /// </summary>
+        /// <param name="variables">The new variables state.</param>
+        /// <returns>The new program state.</returns>
         public SymbolicProgramState<T> WithVariables(ImmutableDictionary<IVariable, SymbolicValue<T>> variables) => 
             new(ProgramCounter, Stack, variables);
 
+        /// <summary>
+        /// Copies the current state and pushes a new value onto the stack. 
+        /// </summary>
+        /// <param name="value">The new value.</param>
+        /// <returns>The new program state.</returns>
         public SymbolicProgramState<T> Push(SymbolicValue<T> value) => 
             new(ProgramCounter, Stack.Push(value), Variables);
 
-        public SymbolicProgramState<T> Pop(out SymbolicValue<T> value) => 
-            new(ProgramCounter, Stack.Pop(out value), Variables);
+        /// <summary>
+        /// Copies the current state and pops the top value from the stack. 
+        /// </summary>
+        /// <param name="value">The popped value.</param>
+        /// <exception cref="StackImbalanceException">Occurs when the stack is empty.</exception>
+        /// <returns>The new program state.</returns>
+        public SymbolicProgramState<T> Pop(out SymbolicValue<T> value)
+        {
+            if (Stack.IsEmpty)
+                throw new StackImbalanceException(ProgramCounter);
+            return new(ProgramCounter, Stack.Pop(out value), Variables);
+        }
 
+        /// <summary>
+        /// Merges two program states together, combining all data sources.
+        /// </summary>
+        /// <param name="otherState">The other program state to merge with.</param>
+        /// <param name="newState">The newly created state.</param>
+        /// <returns><c>true</c> if the state has changed, <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentException">Occurs when the program counters do not match.</exception>
+        /// <exception cref="StackImbalanceException">Occurs when the stack heights do not match.</exception>
         public bool MergeStates(in SymbolicProgramState<T> otherState, out SymbolicProgramState<T> newState)
         {
             if (ProgramCounter != otherState.ProgramCounter)
