110be08cae974f72e7bed33626638591ea297bd6
Washi <washi1337@protonmail.com>
2021-04-07 16:43:13 +0200

    Extract RemoveIncomingEdge method.

--- src/Core/Echo.DataFlow/DataFlowNode.cs
+++ src/Core/Echo.DataFlow/DataFlowNode.cs
@@ -108,7 +108,7 @@ namespace Echo.DataFlow
         /// Obtains a collection of nodes that depend on this node.
         /// </summary>
         /// <returns>The dependant nodes.</returns>
-        public IEnumerable<DataFlowNode<TContents>> GetDependants()=> IncomingEdges
+        public IEnumerable<DataFlowNode<TContents>> GetDependants() => IncomingEdges
             .Select(e => e.Dependent)
             .Distinct();
         
@@ -130,25 +130,8 @@ namespace Echo.DataFlow
         {
             // Remove edges from dependent nodes.
             while (IncomingEdges.Count > 0)
-            {
-                var edge = IncomingEdges[0];
-                switch (edge.DataSource.Type)
-                {
-                    case DataDependencyType.Stack:
-                        foreach (var dependency in edge.Dependent.StackDependencies)
-                            dependency.Remove(this);
-                        break;
+                RemoveIncomingEdge(IncomingEdges[0]);
 
-                    case DataDependencyType.Variable:
-                        foreach (var entry in edge.Dependent.VariableDependencies)
-                            entry.Value.Remove(this);
-                        break;
-
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            
             // Clear dependency nodes.
             foreach (var dependency in StackDependencies)
                 dependency.Clear();
@@ -156,7 +139,34 @@ namespace Echo.DataFlow
                 entry.Value.Clear();
         }
 
+        private static void RemoveIncomingEdge(DataFlowEdge<TContents> edge)
+        {
+            switch (edge.DataSource.Type)
+            {
+                case DataDependencyType.Stack:
+                    foreach (var dependency in edge.Dependent.StackDependencies)
+                    {
+                        if (dependency.Remove((StackDataSource<TContents>) edge.DataSource))
+                            break;
+                    }
+
+                    break;
+
+                case DataDependencyType.Variable:
+                    foreach (var entry in edge.Dependent.VariableDependencies)
+                    {
+                        if (entry.Value.Remove((VariableDataSource<TContents>) edge.DataSource))
+                            break;
+                    }
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         /// <inheritdoc />
-        public override string ToString() => $"{Id} ({Contents})";
+        public override string ToString() => $"{Id:X8} ({Contents})";
     }
 }
