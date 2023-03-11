b3b4b63635ce8db25082dea57ce5f9cd0a443267
Washi <washi1337@protonmail.com>
2023-03-11 14:42:39 +0100

    Add ldsflda handler.

---
+++ src/Platforms/Echo.Platforms.AsmResolver/Emulation/Dispatch/ObjectModel/LdsFldaHandler.cs
@@ -0,0 +1,23 @@
+using AsmResolver.DotNet;
+using AsmResolver.PE.DotNet.Cil;
+using Echo.Platforms.AsmResolver.Emulation.Stack;
+
+namespace Echo.Platforms.AsmResolver.Emulation.Dispatch.ObjectModel
+{
+    /// <summary>
+    /// Implements a CIL instruction handler for <c>ldsflda</c> operations.
+    /// </summary>
+    [DispatcherTableEntry(CilCode.Ldsflda)]
+    public class LdsFldaHandler : FallThroughOpCodeHandler
+    {
+        /// <inheritdoc />
+        protected override CilDispatchResult DispatchInternal(CilExecutionContext context, CilInstruction instruction)
+        {
+            var field = (IFieldDescriptor) instruction.Operand!;
+            var address = context.Machine.ValueFactory.RentNativeInteger(
+                context.Machine.StaticFieldStorage.GetFieldAddress(field));
+            context.CurrentFrame.EvaluationStack.Push(new StackSlot(address, StackSlotTypeHint.Integer));
+            return CilDispatchResult.Success();
+        }
+    }
+}
